63b06f18c8f7b01dc168af0cbbed2ba7df5ec18f
saraalwayslikeworld <1378761528@qq.com>
2018-03-18 18:31:16 +0800

    node 服务器

--- index.js
+++ index.js
@@ -3,11 +3,48 @@ var path = require('path')
 var fs = require('fs')
 var url = require('url')
 
+//创建服务器
 var server = http.createServer(function(req,res){
     console.log('服务器成功')
-    staticRoot(path.join(__dirname,'sample'),req,res)
+    pathRoute(req,res)
+    //staticRoot(path.join(__dirname,'sample'),req,res)
 })
 server.listen(8080)
+
+
+//服务器可反应的路由
+var routes = {
+    "/getsrc":function(req,res){
+        res.end(JSON.stringify({"src":"http://123.com/1.mp3"}))
+    },
+    "/getpics":function(req,res){
+        console.log('pic is not loaded')
+    },
+    "/getmusic":function(req,res){
+        res.end('name='+req.body.name)
+    }
+} 
+//处理路由
+function pathRoute(req,res){
+    var pathObj = url.parse(req.url,true)
+    var handle = routes[pathObj.pathname]
+    if(handle){
+        console.log(req.query)
+        req.query = pathObj.query
+        //解析post信息
+        var body=''
+        req.on('data',function(chunk){
+            body += chunk
+        })
+        req.on('end',function(){
+            req.body = parseBody(body)
+            handle(req,res) 
+        })
+    }else{
+        staticRoot(path.join(__dirname,'sample'),req,res)
+    }
+}
+//处理静态目录
 function staticRoot(staticPath,req,res){
     var pathObj = url.parse(req.url,true)
     if(pathObj.pathname === '/'){
@@ -28,3 +65,11 @@ function staticRoot(staticPath,req,res){
            } 
     })
 }
+function parseBody(body){
+    console.log(body)
+    var obj = {}
+    body.split('&').forEach(function(str){
+      obj[str.split('=')[0]] = str.split('=')[1]
+    })
+    return obj
+  }
--- sample/js/b.js
+++ sample/js/b.js
@@ -1,1 +1,12 @@
-alert('hello')
+
+var xhr = new XMLHttpRequest()
+xhr.open('POST','/getmusic',true)
+xhr.onload = function(req,res){
+    if((xhr.status>=200 && xhr.status<300) ||xhr.status===304){
+        console.log(xhr.responseText)
+    }
+}
+xhr.onerror = function(){
+    console.log('服务器异常')
+}
+xhr.send('name=hello')
